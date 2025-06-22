6c55dbc24318a3ff26eafb3d806bf43662a2855e
Jeevavetrivel <jeevasoulmaker@gmail.com>
2025-06-22 20:32:14 +0530

    initial commit

---
+++ framerusercontent.com/sites/2pkGktMjlwIu1SUo1uMsN6/chunk-FS54IWVE.mjs
@@ -0,0 +1,2 @@
+function a(e,t){return{description:"I\u2019m a passionate web developer and UI/UX designer dedicated to building visually stunning and highly functional digital experiences. ",favicon:"https://framerusercontent.com/assets/zKD2F1WarP0E531PEXdyUgYQPxE.png",robots:"max-image-preview:large",title:"Jo2"}}export{a};
+//# sourceMappingURL=chunk-FS54IWVE.mjs.map
